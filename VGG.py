95d0a620e3cc0cd7e52a45f09705a0dae38f629a
yimenhfeifei <fgao@lamar.edu>
2019-04-18 15:03:25 -0500

    Add files via upload
    VGG structure

--- VGG.py
+++ VGG.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import tools
 import tensorflow.contrib.slim as slim
 
-
+#%% TO get better tensorboard figures!
 
 
 def VGG16N(x, n_classes, is_pretrain=True):
@@ -14,51 +14,59 @@ def VGG16N(x, n_classes, is_pretrain=True):
 
         x = tools.conv('conv1_1', x, 64, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)   
         x = tools.conv('conv1_2', x, 64, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
+#        x = tf.nn.dropout(x, 0.5) 
         with tf.name_scope('pool1'):    
             x = tools.pool('pool1', x, kernel=[1,2,2,1], stride=[1,2,2,1], is_max_pool=True)
             
+            
         x = tools.conv('conv2_1', x, 128, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)    
         x = tools.conv('conv2_2', x, 128, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
+#        x = tf.nn.dropout(x, 0.5) 
         with tf.name_scope('pool2'):    
             x = tools.pool('pool2', x, kernel=[1,2,2,1], stride=[1,2,2,1], is_max_pool=True)
-         
+            
             
 
         x = tools.conv('conv3_1', x, 256, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
         x = tools.conv('conv3_2', x, 256, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
         x = tools.conv('conv3_3', x, 256, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
+       # x = tf.nn.dropout(x, 0.5) 
         with tf.name_scope('pool3'):
             x = tools.pool('pool3', x, kernel=[1,2,2,1], stride=[1,2,2,1], is_max_pool=True)
             
 
-        x = tools.conv('conv4_1', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
-        x = tools.conv('conv4_2', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
-        x = tools.conv('conv4_3', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
+        x = tools.conv('conv4_1', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=True)
+        x = tools.conv('conv4_2', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=True)
+        x = tools.conv('conv4_3', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=True)
+       # x = tf.nn.dropout(x, 0.5) 
         with tf.name_scope('pool4'):
             x = tools.pool('pool4', x, kernel=[1,2,2,1], stride=[1,2,2,1], is_max_pool=True)
-        
+            
 
-        x = tools.conv('conv5_1', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
-        x = tools.conv('conv5_2', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
-        x = tools.conv('conv5_3', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=False)
+        x = tools.conv('conv5_1', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=True)
+        x = tools.conv('conv5_2', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=True)
+        x = tools.conv('conv5_3', x, 512, kernel_size=[3,3], stride=[1,1,1,1], is_pretrain=True)
+        x = tf.nn.dropout(x, 0.5) 
         with tf.name_scope('pool5'):
             x = tools.pool('pool5', x, kernel=[1,2,2,1], stride=[1,2,2,1], is_max_pool=True)            
         
         
-        x = tools.FC_layer('fc6', x, out_nodes=4096)        
+        x = tools.FC_layer('fc6', x, out_nodes=4096)   
+        x = tf.nn.dropout(x, 0.5) 
         #with tf.name_scope('batch_norm1'):
-        x = tools.batch_norm(x)           
+#        x = tools.batch_norm(x)           
         x = tools.FC_layer('fc7', x, out_nodes=4096)        
         #with tf.name_scope('batch_norm2'):
-        x = tools.batch_norm(x) 
-#        x = tf.layer.dropout(x, rate=0.5,training=True)            
+        x = tf.nn.dropout(x,0.5) 
+#        x = tools.batch_norm(x) 
+                   
         x = tools.FC_layer('fc8', x, out_nodes=n_classes)
     
         return x
 
 
 
-
+#%%
 
 
 
